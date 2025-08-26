2cf16762d59fc836e3e4a2fbeedc237638ed6e4d
Zhenya Sugonyaev <73897520+ZhenyaYoratt@users.noreply.github.com>
2025-08-26 18:12:31 +0500

    Changed to WinUI 3 (C#)

--- venv.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system('pip install --upgrade virtualenv')
-os.system('virtualenv venv --python=python3.11.9')
-
-print(' Virtual environment created successfully. ')
-print(' Activate the venv using this command:  .\venv\Scripts\activate')
